89024d34a9c49ec77608d40023eae97f11d6baf2
paulrwd85@gmail.com <paulrwd85@gmail.com>
2021-03-30 15:52:24 +0300

    123321

--- PythonLearning/PythonLearning.py
+++ PythonLearning/PythonLearning.py
@@ -1,13 +1,1 @@
-print("123")
-i = 1
-j = "qwerty"
-print(type(i), i)
-print(type(j))
-k = 3,5
-l = -3.5
-print(type(k), k[0])
-print(type(l), l)
-first_number = "3"
-second_number = 3
-third_number = int(first_number) + second_number
-print(third_number)
+print(5**1000)

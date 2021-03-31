f6532eeda9fef19ce0e40137e11c56286e2efd38
paulrwd85@gmail.com <paulrwd85@gmail.com>
2021-03-31 11:20:53 +0300

    3103_1

--- PythonLearning/PythonLearning.py
+++ PythonLearning/PythonLearning.py
@@ -1,1 +1,13 @@
-print(5**1000)
+import yfinance
+def foo(t: int, y: int) -> int:
+    print(t + y)
+    return "356"
+
+ticker("SBER")
+A = range(0, 101, 1)
+for x in A:
+    print(x)
+ttt = foo('46', '678')
+ccc= ''
+
+

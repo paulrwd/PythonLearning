d57cf19e99220a3a1193a68d225120804596fd0b
paulrwd85@gmail.com <paulrwd85@gmail.com>
2021-04-08 12:26:14 +0300

    0804_1

--- PythonLearning/PythonLearning.py
+++ PythonLearning/PythonLearning.py
@@ -1,5 +1,25 @@
-A = (1, 2 , 3, "345", [1,2,3,"555"])
-B = A
-(B[4])[0] = 10
-print(A)
-print(B)
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+window = pygame.display.set_mode((800,600))
+pygame.display.set_caption("Window")
+black = (0,0,0)
+blue = (0,0,205)
+done = False
+x = 0
+
+while not done:
+    x += 1
+        
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done  = True
+
+    pygame.draw.rect(window,blue,(300 / x, 20*x ,100 + x , 10 * x))
+    pygame.display.flip()
+
+pygame.quit()
+
